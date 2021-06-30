6ef04597011c9c5378fd5f4d0eee1a5da76dceee
EntityFX <artem.solopiy@gmail.com>
2021-06-30 16:17:32 +0300

    Added mulss ns4 results

--- src/E2KSP_Test.cpp
+++ src/E2KSP_Test.cpp
@@ -94,12 +94,81 @@ static uint64_t SSE_S_IR8_##name( CounterType LoopCount,\
 
 #define SSE_S_IR8(op,name) SSE_S_IR8_0(op, #op, name)
 
+
+#define SSE_S_IRS4_8(op) \
+  A = op(A,A);          \
+  B = op(B,B);	        \
+  C = op(C,C);	        \
+  D = op(D,D);	        \
+  E = op(E,E);	        \
+  F = op(F,F);	        \
+  G = op(G,G);	        \
+  H = op(H,H);
+  
+#define SSE_S_IRS4_0(op, op_str, name) \
+static uint64_t SSE_S_IRS4_##name( CounterType LoopCount,\
+                     float answer, float f0, float f1 ) \
+{                                                       \
+  float	ret0, ret1, ret2, ret3, ret4, ret5, ret6, ret7;	\
+  float fp0 = f0;                                       \
+  float fp1 = f1;                                      \
+    TimerClass	timer;			                              \
+    timer.Begin();				                                \
+  __m128 A = _mm_load1_ps (&fp0);                        \
+  __m128 B = _mm_load1_ps (&fp0);                        \
+  __m128 C = _mm_load1_ps (&fp0);                        \
+  __m128 D = _mm_load1_ps (&fp0);                        \
+  __m128 E = _mm_load1_ps (&fp1);                        \
+  __m128 F = _mm_load1_ps (&fp1);                        \
+  __m128 G = _mm_load1_ps (&fp1);                        \
+  __m128 H = _mm_load1_ps (&fp1);                        \
+  for (CounterType loop = 0; loop < LoopCount; loop++) {\
+    SSE_S_IRS4_8( op )	                                  \
+    SSE_S_IRS4_8( op )	                                  \
+    SSE_S_IRS4_8( op )	                                  \
+    SSE_S_IRS4_8( op )	                                  \
+    SSE_S_IRS4_8( op )	                                  \
+  }                                                     \
+  ret0 = _mm_cvtss_f32(A);                              \
+  ret1 = _mm_cvtss_f32(B);                              \
+  ret2 = _mm_cvtss_f32(C);                              \
+  ret3 = _mm_cvtss_f32(D);                              \
+  ret4 = _mm_cvtss_f32(E);                              \
+  ret5 = _mm_cvtss_f32(F);                              \
+  ret6 = _mm_cvtss_f32(G);                              \
+  ret7 = _mm_cvtss_f32(H);                              \
+    timer.End();		                                      \
+    timer.Dump( op_str );                                 \
+    FL_LOG( "%f %f %f %f %f %f %f %f\n",                  \
+       ret0, ret1, ret2, ret3, ret4, ret5, ret6, ret7 );\
+    check_result( ret0, answer );	                        \
+    check_result( ret1, answer );	                        \
+    check_result( ret2, answer );	                        \
+    check_result( ret3, answer );	                        \
+    check_result( ret4, answer );	                        \
+    check_result( ret5, answer );	                        \
+    check_result( ret6, answer );	                        \
+    check_result( ret7, answer );	                        \
+    return	timer.Result();	                              \
+}
+
+#define SSE_S_IRS4(op,name) SSE_S_IRS4_0(op, #op, name)
+
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
 
 static const char*	Instruction_Title[]= {
     "fmuls (mulss) (32bit x1) n8",
     "fadds (addss) (32bit x1) n8",
+    
+    "fmulps (mulps) (32bit x4) n8",
+    "faddps (addps) (32bit x4) n8",
+    
+    "fmulss (mulss) (32bit x1) ns4",
+    "faddss (addss) (32bit x1) ns4",
+
+    "fmulps (mulps) (32bit x4) ns4",
+    "faddps (addps) (32bit x4) ns4",
 };
 
 //-----------------------------------------------------------------------------
@@ -114,6 +183,9 @@ FloatTest::FloatTest()
 
     SetOp( RESULT_SSE_MULSS_IR8,		PER_LOOP_INST * 1, 1	);
     SetOp( RESULT_SSE_ADDSS_IR8,		PER_LOOP_INST * 1, 1	);
+    
+    SetOp( RESULT_SSE_MULSS_IRS4,		PER_LOOP_INST * 1, 1	);
+    SetOp( RESULT_SSE_ADDSS_IRS4,		PER_LOOP_INST * 1, 1	);
 }
 
 //-----------------------------------------------------------------------------
@@ -122,6 +194,9 @@ FloatTest::FloatTest()
 
 SSE_S_IR8( _mm_mul_ss,  mulss_ir8 );
 SSE_S_IR8( _mm_add_ss,  addss_ir8 );
+
+SSE_S_IRS4( _mm_mul_ss,  mulss_irs4 );
+SSE_S_IRS4( _mm_add_ss,  addss_irs4 );
 
 void FloatTest::Run()
 {
@@ -142,6 +217,15 @@ void FloatTest::Run()
 
   //------------------------------------------------------
   //------------------------------------------------------
+  
+    //------------------------------------------------------
+    // IRS4
+    //------------------------------------------------------
+    SetResult( RESULT_SSE_MULSS_IRS4,	SSE_S_IRS4_mulss_irs4( Loop, 0.0f, 0.9999f, 0.9998f) );
+    Progress++;
+
+    SetResult( RESULT_SSE_ADDSS_IRS4,	SSE_S_IRS4_addss_irs4( Loop, std::numeric_limits<float>::infinity(),  1e-35f, 1e-35f) );
+    Progress++;
 
     DoneFlag= true;
 }
